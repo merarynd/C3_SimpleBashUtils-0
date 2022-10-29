2c337e6e2afa45a79fb3f6806e422e4e0ab321a8
Mera Ryndoon <merarynd@et-c6.kzn.21-school.ru>
2022-10-30 01:30:31 +0300

    1

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,21 +1,29 @@
-#include<stdio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int fop();
-int output();
+int main(int argc, char* argv[]) {
+    int fd = 0, rb = 1;
+    char c = 0;
 
-int main(int argc, char *argv[]) {
-    int i = 0;
-    for(i = 0; i < argc; i++);
-    
-int fop();
-int output();
-return 0;
-}
-
-int fop() {
-
-}
-
-int output() {
-
+    fd = open(argv[2], O_RDONLY);
+    if (fd < 0) {
+        printf("fail");
+    } else {
+        while (rb > 0) {
+            rb = read(fd, &c, 1);
+            if (rb < 0) {
+                printf("failrrrrr");
+                break;
+            }
+            if (rb == 0) {
+                break;
+            }
+            printf("%c", c);
+        }
+    }
+    if(argv[1] = 45){
+        printf("rrrr");
+    }
+    return 0;
 }

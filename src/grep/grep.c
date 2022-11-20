367ec2146224d1f10941a968f5b75a7827d4c659
Mera Ryndoon <merarynd@et-c6.kzn.21-school.ru>
2022-11-20 19:20:53 +0300

    КОНЕЕЕЕЕЦgit add .git add .git add .git add .ls

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -1,156 +1,51 @@
-#include <getopt.h>
-#include <regex.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define D_SIMVOL 4096
-
-void parser();
-void grep();
-void reader();
-
-struct options {
-  int e;
-  int i;
-  int v;
-  int c;
-  int l;
-  int n;
-  int h;
-  int s;
-  int f;
-  int o;
-};
-
-int main(int argc, char *argv[]) {
-  char f_pattern[D_SIMVOL] = {0};
-  struct options flags = {0};
-  parser(argc, argv, &flags, f_pattern);
-  // flag(&flags);
-  char file[D_SIMVOL] = {0};
-  int rev = 0;
-  if (!flags.f && !flags.e) {
-    snprintf(file, D_SIMVOL, "%s", argv[optind++]);
-  }
-  if (argc - optind > 1) {
-    rev = 1;
-  }
-  for (int i = optind; i < argc; i++) {
-    grep(flags, file, argv[i]);
-  }
-  return 0;
-}
-void parser(int argc, char *argv[], struct options *flags, char *f_pattern) {
-  int opt = 0;
-  const char *flag_options = "e:ivclnhsf:o";
-  while ((opt = getopt_long(argc, argv, flag_options, NULL, NULL)) != -1) {
-    switch (opt) {
-      case 'e':
-        flags->e = 1;
-        sprintf(f_pattern, "%s", optarg);
-        break;
-      case 'i':
-        flags->e = 1;
-        break;
-      case 'v':
-        flags->e = 1;
-        break;
-      case 'c':
-        flags->e = 1;
-        break;
-      case 'l':
-        flags->e = 1;
-        break;
-      case 'n':
-        flags->e = 1;
-        break;
-      case 'h':
-        flags->e = 1;
-        break;
-      case 's':
-        flags->e = 1;
-        break;
-      case 'f':
-        flags->e = 1;
-        sprintf(f_pattern, "%s", optarg);
-        break;
-      case 'o':
-        flags->e = 1;
-        break;
-      default:
-        fprintf(stderr, "grep: illegal option -- %c\n", opt);
-        printf("usage: grep [eivclnhsfo] [file ...]\n");
-        break;
-    }
-  }
-}
-
-// void flag(struct options *flags) {
-//   printf("flag_e = %d\n", options.e);
-//   printf("flag_i = %d\n", options.i);
-//   printf("flag_v = %d\n", options.v);
-//   printf("flag_c = %d\n", options.c);
-//   printf("flag_l = %d\n", options.l);
-//   printf("flag_n = %d\n", options.n);
-//   printf("flag_h = %d\n", options.h);
-//   printf("flag_s = %d\n", options.s);
-//   printf("flag_f = %d\n", options.f);
-//   printf("flag_o = %d\n", options.o);
-//   printf("\n");
-// }
-
-// void flag_f(char *pattern, char *namef) {
-//   int buf, i = 0;
-//   FILE *fp = fopen(namef, "r");
-//   if (fp == NULL) {
-//     fprintf(stderr, "grep: %s: No such file or directory\n", namef);
-//     i = -1;
-//   } else {
-//     int rev;
-//     while ((buf = fgetc(fp)) != EOF) {
-//       if (rev == 13 || rev == 10) pattern[i++] = '|';
-//       if (rev != 13 && rev != 10) pattern[i++] = rev;
-//     }
-//   }
-//   if (pattern[i - 1] == '|') {
-//     pattern[i - 1] = '\0';
-//   }
-//   fclose(fp);
-//   fp = NULL;
-// }
-
-void grep(struct options flags, char *pattern, char *namef) {
-  int fl = REG_EXTENDED;
+void grep(struct options *flags, char *pattern, char *namef) {
   char text[D_SIMVOL] = {0};
   regex_t reg;
   FILE *fp;
   fp = fopen(namef, "r");
   if (fp != NULL) {
-    regcomp(&reg, pattern, fl);
-    reader(flags, fp, reg, namef);
+    if (flags->i) {
+      regcomp(&reg, pattern, REG_ICASE);
+    } else {
+      regcomp(&reg, pattern, REG_EXTENDED);
+    }
+    regmatch_t pmatch[1];
+    int line = 0, nl = 1;
+    while (fgets(namef, D_SIMVOL - 1, fp) != NULL) {
+      int match = 0;
+      int success = regexec(&reg, text, 1, pmatch, 0);
+      if (strchr(text, '\n') == NULL) {
+        strcat(text, "\n");
+      }
+      if (success == 0 && !flags->v) {
+        match = 1;
+      }
+      if (success == REG_NOMATCH && flags->v) {
+        match = 1;
+      }
+      if (match && !flags->l && !flags->c && flags->n) {
+        printf("%d:", nl);
+      }
+
+      if (match && !flags->l && !flags->c && !flags->o) {
+        printf("%s", text);
+      }
+      if (match && flags->o) {
+        for (int i = pmatch[0].rm_so; i < pmatch[0].rm_eo; i++) {
+          printf("%c", text[i]);
+        }
+        printf("\n");
+      }
+      line += match;
+      nl++;
+    }
+    if (flags->l && line > 0) {
+      printf("%s\n", namef);
+    }
+    if (flags->c && !flags->l) {
+      printf("%d\n", line);
+    }
     regfree(&reg);
     fclose(fp);
-  }
-}
-
-void reader(struct options flags, FILE *fp, regex_t reg, char *file) {
-  char text[D_SIMVOL] = {0};
-  regmatch_t pmatch[1];
-  while (fgets(text, D_SIMVOL - 1, fp) != NULL) {
-    int match = 0;
-    int success = regexec(&reg, text, 1, pmatch, 0);
-    if (success == 0 && !flags.v) {
-      match = 1;
-    }
-    if (match && !flags.l && !flags.c && !flags.o) {
-      printf("%s", text);
-    }
-    if (match && flags.o) {
-      for (int i = pmatch[0].rm_so; i < pmatch[0].rm_eo; i++) {
-        printf("%c", text[i]);
-      }
-      printf("\n");
-    }
   }
 }
--- src/grep/s21_grep.c
+++ src/grep/s21_grep.c
@@ -1,18 +1,13 @@
-#include <fcntl.h>
+
 #include <getopt.h>
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #define D_SIMVOL 4096
-
 void parser();
-void reader();
-void flag();
-// void grep();
-
-typedef struct options {
+void grep();
+struct options {
   int e;
   int i;
   int v;
@@ -23,92 +18,135 @@ typedef struct options {
   int s;
   int f;
   int o;
-} opt;
-
+};
 int main(int argc, char *argv[]) {
-  char pattern[D_SIMVOL] = {0};
-  opt options = {0};
-  parser(argc, argv, &options);
-  reader(pattern, &options);
-  flag(options);
-  // grep(argv, pattern, &options);
+  char f_pattern[D_SIMVOL] = {0};
+  struct options flags = {0};
+  parser(argc, argv, &flags, f_pattern);
+  // flag(&flags);
+  char file[D_SIMVOL] = {0};
+  if (!flags.f && !flags.e) {
+    snprintf(file, D_SIMVOL, "%s", argv[optind++]);
+  }
+  if (argc - optind > 1) {
+  }
+  for (int i = optind; i < argc; i++) {
+    grep(&flags, file, argv[i]);
+  }
   return 0;
 }
-
-void parser(int argc, char *argv[], opt *options) {
+void parser(int argc, char *argv[], struct options *flags, char *f_pattern) {
   int opt = 0;
-  char str[D_SIMVOL] = {0};
-  while ((opt = getopt(argc, argv, "e:ivclnhsf:o")) != -1) {
+  const char *flag_options = "e:ivclnhsf:o";
+  while ((opt = getopt_long(argc, argv, flag_options, NULL, NULL)) != -1) {
     switch (opt) {
       case 'e':
-        options->e = 1;
-        sprintf(str, "%s %d %c", optarg);
+        flags->e = 1;
+        sprintf(f_pattern, "%s", optarg);
         break;
       case 'i':
-        options->i = 1;
+        flags->i = 1;
         break;
       case 'v':
-        options->v = 1;
+        flags->v = 1;
         break;
       case 'c':
-        options->c = 1;
+        flags->c = 1;
         break;
       case 'l':
-        options->l = 1;
+        flags->l = 1;
         break;
       case 'n':
-        options->n = 1;
+        flags->n = 1;
         break;
       case 'h':
-        options->h = 1;
+        flags->h = 1;
         break;
       case 's':
-        options->s = 1;
+        flags->s = 1;
         break;
       case 'f':
-        options->f = 1;
-        sprintf(str, "%s %d %c", optarg);
+        flags->f = 1;
+        sprintf(f_pattern, "%s", optarg);
         break;
       case 'o':
-        options->o = 1;
+        flags->o = 1;
         break;
       default:
         fprintf(stderr, "grep: illegal option -- %c\n", opt);
         printf("usage: grep [eivclnhsfo] [file ...]\n");
-        exit(1);
+        break;
     }
   }
 }
 
-void flag(opt options) {
-  printf("flag_e = %d\n", options.e);
-  printf("flag_i = %d\n", options.i);
-  printf("flag_v = %d\n", options.v);
-  printf("flag_c = %d\n", options.c);
-  printf("flag_l = %d\n", options.l);
-  printf("flag_n = %d\n", options.n);
-  printf("flag_h = %d\n", options.h);
-  printf("flag_s = %d\n", options.s);
-  printf("flag_f = %d\n", options.f);
-  printf("flag_o = %d\n", options.o);
-  printf("\n");
+void grep(struct options *flags, char *pattern, char *namef) {
+  // int fl = REG_EXTENDED;
+  char text[D_SIMVOL] = {0};
+  regex_t reg;
+  FILE *fp;
+  fp = fopen(namef, "r");
+  if (fp != NULL) {
+    if (flags->i) {
+      regcomp(&reg, pattern, REG_ICASE);
+    } else {
+      regcomp(&reg, pattern, REG_EXTENDED);
+    }
+    regmatch_t pmatch[1];
+    int line = 0, nl = 1;
+    while (fgets(text, D_SIMVOL - 1, fp) != NULL) {
+      int match = 0;
+      int success = regexec(&reg, text, 1, pmatch, 0);
+      if (strchr(text, '\n') == NULL) {
+        strcat(text, "\n");
+      }
+      if (success == 0 && !flags->v) {
+        match = 1;
+      }
+      if (success == REG_NOMATCH && flags->v) {
+        match = 1;
+      }
+      if (match && !flags->l && !flags->c && flags->n) {
+        printf("%d:", nl);
+      }
+      if (match && !flags->l && !flags->c && !flags->o) {
+        printf("%s", text);
+      }
+      if (match && flags->o) {
+        for (int i = pmatch[0].rm_so; i < pmatch[0].rm_eo; i++) {
+          printf("%c", text[i]);
+        }
+        printf("\n");
+      }
+      line += match;
+      nl++;
+    }
+    if (flags->l && line > 0) {
+      printf("%s\n", namef);
+    }
+    if (flags->c && !flags->l) {
+      printf("%d\n", line);
+    }
+    regfree(&reg);
+    fclose(fp);
+  }
 }
 
-void reader(char *pattern, char *argv[]) {
-  int buf, i = 0;
-  FILE *fp = fopen(argv[i], "r");
-  if (fp == NULL) {
-    fprintf(stderr, "grep: %s: No such file or directory\n", argv[i]);
-  } else {
-    int rev;
-    while ((buf = fgetc(fp)) != EOF) {
-      if (rev == 13 || rev == 10) pattern[i++] = '|';
-      if (rev != 13 && rev != 10) pattern[i++] = rev;
-    }
-  }
-  if (pattern[i - 1] == '|') {
-    pattern[i - 1] = '\0';
-  }
-  fclose(fp);
-  fp = NULL;
-}
+// void(char *pattern, char *argv[]) {
+//   int buf, i = 0;
+//   FILE *fp = fopen(argv[i], "r");
+//   if (fp == NULL) {
+//     fprintf(stderr, "grep: %s: No such file or directory\n", argv[i]);
+//   } else {
+//     int rev;
+//     while ((buf = fgetc(fp)) != EOF) {
+//       if (rev == 13 || rev == 10) pattern[i++] = '|';
+//       if (rev != 13 && rev != 10) pattern[i++] = rev;
+//     }
+//   }
+//   if (pattern[i - 1] == '|') {
+//     pattern[i - 1] = '\0';
+//   }
+//   fclose(fp);
+//   fp = NULL;
+// }
